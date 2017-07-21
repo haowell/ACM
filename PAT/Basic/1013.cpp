4e5e10eafee9174c6a324de9773d37139424f058
haowell <928066986@qq.com>
2017-07-21 15:47:17 +0800

    PAT Basic 1013 1014 complete

---
+++ PAT/Basic/1013.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+//判断是否是素数
+bool isPrime(int num)
+{
+	if(num==2)
+	{
+		return true;
+	}
+	else if(num==1||num%2==0) 
+	{
+		return false;
+	}	
+	else 
+	{
+		//遍历小于等于a的奇数，是否能被整除，其中a=sqrt(num)
+		for(int i=3;i<=sqrt(num);i+=2)
+		{
+			if(num%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
+int main()
+{
+	int x=3,m,n;
+
+	//计算存储第1个素数到第10000个素数
+	int a[10005];
+	a[0]=2;
+	for(int i=1;i<10000;x+=2)
+	{
+		if(isPrime(x))
+		{
+			a[i]=x;
+			i++;
+		}
+	}
+	while(cin>>m>>n)
+	{
+		for(int i=m;i<=n;i++)
+		{
+			if((i-m)%10==0)
+			{
+				cout<<a[i-1];
+			}
+			else if((i-m)%10==9)
+			{
+				cout<<" "<<a[i-1]<<"\n";
+			}
+			else
+			{
+				cout<<" "<<a[i-1];
+			}
+		}
+	}
+}
---
+++ PAT/Basic/1014.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+void Week(char week)
+{
+	switch(week)
+	{
+		case 'A':
+			cout<<"MON ";
+			break;
+		case 'B':
+			cout<<"TUE ";
+			break;
+		case 'C':
+			cout<<"WED ";
+			break;
+		case 'D':
+			cout<<"THU ";
+			break;
+		case 'E':
+			cout<<"FRI ";
+			break;
+		case 'F':
+			cout<<"SAT ";
+			break;
+		case 'G':
+			cout<<"SUN ";
+			break;
+	}
+}
+void Hour(char hour)
+{
+	if(hour>='0'&&hour<='9')
+	{
+		cout<<"0"<<(hour-'0')<<":";
+	}
+	else
+	{
+		cout<<(hour-'A')+10<<":";
+	}
+}
+void Minute(int minute)
+{
+	if(minute<10)
+	{
+		cout<<"0"<<minute<<"\n";
+	}
+	else
+	{
+		cout<<minute<<"\n";
+	}
+}
+int main()
+{
+	string a1,a2,b1,b2;
+	int alen,blen,judge,minute;
+	char week,hour;
+	while(cin>>a1>>a2>>b1>>b2)
+	{
+		judge=0;
+		alen=min(a1.length(),a2.length());
+		for(int i=0;i<alen;i++)
+		{
+			//前两个字符串第一对相等的符合条件大写字母
+			if(judge==0&&a1[i]>='A'&&a1[i]<='G'&&a1[i]==a2[i])
+			{
+				week=a1[i];
+				judge++;
+			}
+
+			//前两个字符串第二对相等的符合条件字符
+			else if(judge==1&&
+			( (a1[i]>='A'&&a1[i]<='N')||
+			(a1[i]>='0'&&a1[i]<='9') )&&
+			a1[i]==a2[i])
+			{
+				hour=a1[i];
+				judge++;
+			}
+		}
+
+		
+		blen=min(b1.length(),b2.length());
+		for(int i=0;i<blen;i++)
+		{
+			//后两个字符串第一对相等的符合条件的字母
+			if(( (b1[i]>='a'&&b1[i]<='z')||
+			(b1[i]>='A'&&b1[i]<='Z') )&&
+			b1[i]==b2[i])
+			{
+				minute=i;
+				break;
+			}
+		}
+		Week(week);
+		Hour(hour);
+		Minute(minute);
+	}	
+}
