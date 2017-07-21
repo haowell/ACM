39184722d381e197cf2ff7fe359449bb984661bc
haowell <928066986@qq.com>
2017-07-21 12:21:35 +0800

    PAT Basic 1011 1012 complete

--- PAT/Basic/1011.cpp
+++ PAT/Basic/1011.cpp
@@ -6,5 +6,22 @@
 using namespace std;
 int main()
 {
+	int n;
 
+	//使用long long int输入
+	long long int a,b,c;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>a>>b>>c;
+		cout<<"Case #"<<i<<": ";
+		if(a+b>c)
+		{
+			cout<<"true\n";
+		}
+		else
+		{
+			cout<<"false\n";
+		}
+	}
 }
---
+++ PAT/Basic/1012.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	//a2num记录A2类型数个数，判断进行加法还是减法操作，同时用来判断是否存在A2类型数
+	//a4num记录A4类型数个数，用来求平均数
+	int n,x,a1=0,a2=0,a3=0,a5=-1,a2num=0,a4num=0;
+	double a4=0.0;
+	cin>>n;
+	while(n--)
+	{
+		cin>>x;
+		switch(x%5)
+		{
+			case 0:
+				if(x%2==0)
+				{
+					a1+=x;
+				}
+				break;
+			case 1:
+				if(a2num%2)
+				{
+					a2-=x;
+				}
+				else
+				{
+					a2+=x;
+				}
+				a2num++;
+				break;
+			case 2:
+				a3++;
+				break;
+			case 3:
+				a4+=x;
+				a4num++;
+				break;
+			case 4:
+				if(x>a5)
+				{
+					a5=x;
+				}
+				break;
+		}
+	}
+	if(a1==0)
+	{
+		cout<<"N";
+	}
+	else
+	{
+		cout<<a1;
+	}
+
+	//只有交错求和的结果可能与初始值相同，因此用是否存在这类数字来判断
+	if(a2num==0)
+	{
+		cout<<" N";
+	}
+	else
+	{
+		cout<<" "<<a2;
+	}
+	if(a3==0)
+	{
+		cout<<" N";
+	}
+	else
+	{
+		cout<<" "<<a3;
+	}
+	if(a4==0.0)
+	{
+		cout<<" N";
+	}
+	else
+	{
+		//精确到小数点后1位
+		cout<<" "<<fixed<<setprecision(1)<<a4/a4num;
+	}
+	if(a5==-1)
+	{
+		cout<<" N";
+	}
+	else
+	{
+		cout<<" "<<a5;
+	}
+}
