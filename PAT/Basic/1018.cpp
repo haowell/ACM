e312412597c08fb1b98cdbe4e2aef99910206058
haowell <928066986@qq.com>
2017-07-25 16:55:41 +0800

    PAT Basic 1018 complete 1019 pass

---
+++ PAT/Basic/1018.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+struct Count
+{
+	//记录胜的次数和某手势胜的次数
+	int C;
+	int B;
+	int J;
+	int win; 
+};
+
+//找出个人胜次最多的手势
+void winmost(Count p)
+{
+	char c;
+	
+	//比较锤子和布的胜次
+	if(p.C>p.B)
+	{
+		//比较锤子和剪刀的胜次
+		if(p.J>p.C)
+		{
+			c='J';
+		}
+		else
+		{
+			c='C';
+		}
+	}
+	else
+	{
+		//比较布和剪刀的胜次
+		if(p.J>p.B)
+		{
+			c='J';
+		}
+		else
+		{
+			c='B';
+		}
+	}
+	cout<<c;
+}
+int main()
+{
+	//初始化甲乙两人胜次，均为0
+	Count jia,yi;
+	jia.B=jia.C=jia.J=jia.win=yi.B=yi.C=yi.J=yi.win=0;
+	int n;
+	char a,b;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a>>b;
+	
+		//甲胜的情况
+		if(a=='C'&&b=='J')
+		{
+			jia.C++;
+			jia.win++;
+		}
+		else if(a=='J'&&b=='B')
+		{
+			jia.J++;
+			jia.win++;
+		}
+		else if(a=='B'&&b=='C')
+		{
+			jia.B++;
+			jia.win++;
+		}
+
+		//乙胜的情况
+		else if(a=='C'&&b=='B')
+		{
+			yi.B++;
+			yi.win++;
+		}
+		else if(a=='J'&&b=='C')
+		{
+			yi.C++;
+			yi.win++;
+		}
+		else if(a=='B'&&b=='J')
+		{
+			yi.J++;
+			yi.win++;
+		}
+	}
+	cout<<jia.win<<" "<<n-jia.win-yi.win<<" "<<yi.win<<"\n";
+	cout<<yi.win<<" "<<n-jia.win-yi.win<<" "<<jia.win<<"\n";
+	winmost(jia);
+	cout<<" ";
+	winmost(yi);
+	cout<<"\n";
+}
---
+++ PAT/Basic/1019.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+char a[4];
+char b[4];
+bool ascend(char a,char b)
+{
+	return a<b;
+}
+bool decend(char a,char b)
+{
+	return a>b;
+}
+void printa()
+{
+	for(int i=0;i<4;i++)
+	{
+		cout<<a[i];
+	}
+}
+void printb()
+{
+	for(int i=0;i<4;i++)
+	{
+		cout<<b[i];
+	}
+}
+void sub()
+{
+	printa();
+	cout<<" - ";
+	printb();
+	cout<<" = ";
+	for(int i=3;i>=0;i--)
+	{
+		if(a[i]<b[i]&&i!=0)
+		{
+			a[i]=a[i]-b[i]+10+'0';
+			a[i-1]-=1;
+		}
+		else
+		{
+			a[i]=a[i]-b[i]+'0';
+		}
+	}
+	printa();
+	cout<<"\n";
+	if(a[0]=='0'&&a[1]=='0'&&a[2]=='0'&&a[3]=='0')
+	{
+		return;
+	}
+	else if(a[0]=='6'&&a[1]=='1'&&a[2]=='7'&&a[3]=='4')
+	{
+		return;
+	}
+	for(int i=0;i<4;i++)
+	{
+		b[i]=a[i];
+	}
+	sort(a,a+4,decend);
+	sort(b,b+4,ascend);
+	sub();
+}
+int main()
+{
+	int n;
+	while(cin>>n)
+	{
+		a[0]=b[0]=(n/1000+'0');
+		a[1]=b[1]=(n%1000/100+'0');
+		a[2]=b[2]=(n%1000%100/10+'0');
+		a[3]=b[3]=(n%1000%100%10+'0');
+		sort(a,a+4,decend);
+		sort(b,b+4,ascend);
+		sub();				
+	}	
+}
