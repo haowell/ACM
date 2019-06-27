3ad8d32a532adeec3a23dcf871ec2d6b92f91744
haowell <928066986@qq.com>
2019-06-27 18:43:34 +0800

    PAT Basic 1019 complete

--- PAT/Basic/1019.cpp
+++ PAT/Basic/1019.cpp
@@ -8,14 +8,20 @@
 using namespace std;
 char a[4];
 char b[4];
+
+//升序排序
 bool ascend(char a,char b)
 {
 	return a<b;
 }
+
+//降序排序
 bool decend(char a,char b)
 {
 	return a>b;
 }
+
+//打印第一个数字
 void printa()
 {
 	for(int i=0;i<4;i++)
@@ -23,6 +29,8 @@ void printa()
 		cout<<a[i];
 	}
 }
+
+//打印第二个数字
 void printb()
 {
 	for(int i=0;i<4;i++)
@@ -30,8 +38,14 @@ void printb()
 		cout<<b[i];
 	}
 }
+
+//执行减法运算
 void sub()
 {
+	//改变数字位置
+	sort(a,a+4,decend);
+	sort(b,b+4,ascend);
+	
 	printa();
 	cout<<" - ";
 	printb();
@@ -50,10 +64,14 @@ void sub()
 	}
 	printa();
 	cout<<"\n";
+	
+	//输出为0时停止
 	if(a[0]=='0'&&a[1]=='0'&&a[2]=='0'&&a[3]=='0')
 	{
 		return;
 	}
+	
+	//输出为6174时停止
 	else if(a[0]=='6'&&a[1]=='1'&&a[2]=='7'&&a[3]=='4')
 	{
 		return;
@@ -62,8 +80,7 @@ void sub()
 	{
 		b[i]=a[i];
 	}
-	sort(a,a+4,decend);
-	sort(b,b+4,ascend);
+	
 	sub();
 }
 int main()
@@ -75,8 +92,6 @@ int main()
 		a[1]=b[1]=(n%1000/100+'0');
 		a[2]=b[2]=(n%1000%100/10+'0');
 		a[3]=b[3]=(n%1000%100%10+'0');
-		sort(a,a+4,decend);
-		sort(b,b+4,ascend);
 		sub();				
 	}	
 }
